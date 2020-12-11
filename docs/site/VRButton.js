7f9b296a622599b8bc7415ec0a304db3bfaf5f16
digireaTamura <digirea.tamura@gmail.com>
2020-12-11 20:30:26 +0900

    site：VR用モジュール更新

--- docs/site/VRButton.js
+++ docs/site/VRButton.js
@@ -41,8 +41,6 @@ class VRButton {
         function showEnterVR(/*device*/) {
             window.log("run showEnterVR ");
 
-            console.log(view.camera.camera3D.quaternion);
-
             function onSessionStarted(session) {
                 window.log("run onSessionStarted ");
 
@@ -256,8 +254,6 @@ window.fixCamLonPos = function () {
 
 /// カメラを水平線方向に向ける
 window.setCamLookHorizon = function () {
-    console.log(view.camera.camera3D.quaternion);
-
     let position = view.camera.camera3D.position;
     let normPosition = new itowns.THREE.Vector3(
         position.x,
@@ -266,17 +262,24 @@ window.setCamLookHorizon = function () {
     );
 
     normPosition.normalize();
+    let upVector = new itowns.THREE.Vector3().copy(normPosition);
     // カメラを地球の中心に向けてから
-    view.camera.camera3D.lookAt(normPosition);
-    view.camera.camera3D.updateMatrix();
-    /*
     // カメラを水平線に向ける
     const q = new itowns.THREE.Quaternion();
     const axis = new itowns.THREE.Vector3(1, 0, 0);
     q.setFromAxisAngle(axis, Math.PI / 2);
+    upVector
+        .applyQuaternion(view.camera.camera3D.quaternion)
+        .applyQuaternion(q);
+
+    view.camera.camera3D.up.copy(upVector);
+    view.camera.camera3D.lookAt(normPosition);
+    view.camera.camera3D.updateMatrix();
+
     view.camera.camera3D.quaternion.multiply(q);
     view.camera.camera3D.updateMatrix();
-*/
+
+    /*
     let mx = new itowns.THREE.Matrix4().copy(view.camera.camera3D.matrix);
     const obj = new itowns.THREE.Object3D();
     obj.applyMatrix4(mx);
@@ -285,6 +288,8 @@ window.setCamLookHorizon = function () {
     view.camera.camera3D.updateMatrix();
     view.camera.camera3D.updateMatrixWorld();
     view.camera.camera3D.updateProjectionMatrix();
+*/
+
     // view.notifyChange(view.camera); //動作に影響がないようなら、コメントアウトのまま（重い
 };
 
